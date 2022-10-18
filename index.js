3e6768ae8ffc93f1d6112c9dbaf6499bb596ffba
SJBauer0 <sjimenezbauer@gmail.com>
2022-10-17 22:25:31 -0500

    Acabar iconos y finalizar el menu

---
+++ index.js
@@ -0,0 +1,57 @@
+let index = 0;
+const color = ["#9AFF4B","white"];
+
+function buttonColor() {
+    document.getElementById("circle").style.backgroundColor= color[index];
+    index = index >= color.length - 1 ? 0 : index + 1;
+}
+
+function buttonColor2() {
+    document.getElementById("circle2").style.backgroundColor= color[index];
+    index = index >= color.length - 1 ? 0 : index + 1;
+}
+
+function buttonColor3() {
+    document.getElementById("circle3").style.backgroundColor= color[index];
+    index = index >= color.length - 1 ? 0 : index + 1;
+}
+
+function buttonColor4() {
+    document.getElementById("circle4").style.backgroundColor= color[index];
+    index = index >= color.length - 1 ? 0 : index + 1;
+}
+
+function buttonColor5() {
+    document.getElementById("circle5").style.backgroundColor= color[index];
+    index = index >= color.length - 1 ? 0 : index + 1;
+}
+
+function buttonColor6() {
+    document.getElementById("circle6").style.backgroundColor= color[index];
+    index = index >= color.length - 1 ? 0 : index + 1;
+}
+
+function buttonColor7() {
+    document.getElementById("circle7").style.backgroundColor= color[index];
+    index = index >= color.length - 1 ? 0 : index + 1;
+}
+
+function buttonColor8() {
+    document.getElementById("circle8").style.backgroundColor= color[index];
+    index = index >= color.length - 1 ? 0 : index + 1;
+}
+
+function buttonColor9() {
+    document.getElementById("circle9").style.backgroundColor= color[index];
+    index = index >= color.length - 1 ? 0 : index + 1;
+}
+
+function buttonColor10() {
+    document.getElementById("circle10").style.backgroundColor= color[index];
+    index = index >= color.length - 1 ? 0 : index + 1;
+}
+
+function buttonColor11() {
+    document.getElementById("circle10").style.backgroundColor= color[index];
+    index = index >= color.length - 1 ? 0 : index + 1;
+}
